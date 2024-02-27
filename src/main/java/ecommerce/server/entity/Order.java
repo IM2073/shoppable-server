11119cfb0ea953b8eedf8c804ad33b8d9c8b29f4
Bryan Matthew <bryanmatthew206@gmail.com>
2024-02-27 12:57:10 +0800

    fix: add @Id annotation in order and orderDetail model

--- src/main/java/ecommerce/server/entity/Order.java
+++ src/main/java/ecommerce/server/entity/Order.java
@@ -1,15 +1,10 @@
 package ecommerce.server.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
-
-import java.util.UUID;
-
+import jakarta.persistence.*;
 @Entity
 @Table(name="orders")
 public class Order {
+    @Id
     @Column(name="id")
     @GeneratedValue
     private Integer id;
--- src/main/java/ecommerce/server/entity/OrderDetail.java
+++ src/main/java/ecommerce/server/entity/OrderDetail.java
@@ -1,13 +1,11 @@
 package ecommerce.server.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="orderDetails")
 public class OrderDetail {
+    @Id
     @Column(name="id")
     @GeneratedValue
     private Integer id;
